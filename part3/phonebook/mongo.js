c184e25e82b110bada91594e79231c383b2365d4
Malik Fajar <fajar.malik@aalto.fi>
2024-04-02 10:45:39 +0300

    Added until 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://fullstack:${password}@cluster0.1bkdqb0.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 5) {
+    Person.find({}).then(persons => {
+        console.log("phonebook:")
+        persons.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({ name, number })
+    person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    })
+}
